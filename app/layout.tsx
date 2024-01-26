ae486f1934931c8131a97521e2d946b376b7664f
albert19_ <albertnanda1902@gmail.com>
2024-01-26 20:45:06 +0800

    create designer context to drag and sort the components

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import { Toaster } from "@/components/ui/toaster";
+import DesignerContextProvider from "@/components/context/DesignerContext";
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -18,10 +19,12 @@ export default function RootLayout({
 		<ClerkProvider>
 			<html lang="en">
 				<body>
-					<ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-						{children}
-						<Toaster />
-					</ThemeProvider>
+					<DesignerContextProvider>
+						<ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+							{children}
+							<Toaster />
+						</ThemeProvider>
+					</DesignerContextProvider>
 				</body>
 			</html>
 		</ClerkProvider>
--- components/fields/TextField.tsx
+++ components/fields/TextField.tsx
@@ -1,27 +1,55 @@
 "use client";
 
 import { MdTextFields } from "react-icons/md";
-import { ElementsType, FormElement } from "../FormElements";
+import { ElementsType, FormElement, FormElementInstance } from "../FormElements";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
 
 const type: ElementsType = "TextField";
+
+type CustomInstance = FormElementInstance & {
+    extraAttributes: typeof extraAttributes
+}
+
+const extraAttributes = {
+    label: "Text field",
+    helperText: "Helper text",
+    required: false,
+    placeHolder: "Value here...",
+}
+
+const DesignerComponent = ({ elementInstance }: { elementInstance: FormElementInstance }) => {
+
+    const element = elementInstance as CustomInstance;
+
+    const { label, required, placeHolder, helperText } = element.extraAttributes
+
+    return (
+        <div className="flex flex-col gap-2 w-full">
+            <Label>
+                {label}
+                {required && "*"}
+            </Label>
+            <Input readOnly disabled placeholder={placeHolder} />
+            {helperText && (
+                <p className="text-muted-foreground text-[0.8rem]">{helperText}</p>
+            )}
+        </div>
+    )
+}
 
 export const TextFieldFormElement: FormElement = {
     type,
     construct: (id: string) => ({
         id,
         type,
-        extraAttributes: {
-            label: "Text field",
-            helperText: "Helper text",
-            required: false,
-            placeHolder: "Value here...",
-        },
+        extraAttributes,
     }),
     designerBtnElement: {
         icon: MdTextFields,
         label: "Text Field",
     },
-    designerComponent: () => <div>Designer Component</div>,
+    designerComponent: DesignerComponent,
     formComponent: () => <div>Form Component</div>,
     propertiesComponent: () => <div>Properties Component</div>
 }
