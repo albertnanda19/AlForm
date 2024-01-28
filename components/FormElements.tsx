625cae186724fa10843b2893f5396d591014744c
albert19_ <albertnanda1902@gmail.com>
2024-01-28 22:31:21 +0800

    add subtitle field element

--- components/FormElements.tsx
+++ components/FormElements.tsx
@@ -1,7 +1,8 @@
+import { SubTitleFieldFormElement } from "./fields/SubTitleField";
 import { TextFieldFormElement } from "./fields/TextField";
 import { TitleFieldFormElement } from "./fields/TitleField";
 
-export type ElementsType = "TextField" | "TitleField";
+export type ElementsType = "TextField" | "TitleField" | "SubTitleField";
 
 export type SubmitFunction = (key: string, value: string) => void;
 
@@ -41,5 +42,6 @@ type FormElementsType = {
 
 export const FormElements: FormElementsType = {
     TextField: TextFieldFormElement,
-    TitleField: TitleFieldFormElement
+    TitleField: TitleFieldFormElement,
+    SubTitleField: SubTitleFieldFormElement
 };
